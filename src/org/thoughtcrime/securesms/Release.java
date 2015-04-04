b5d8d23a4a629adf0500ae4c281169e6c5da2ee5
Elias Naur <elias.naur@gmail.com>
2015-04-04 19:18:46 +0200

    revert GCM check (new registration window is replacing the old) and update local server URL

--- src/org/thoughtcrime/securesms/Release.java
+++ src/org/thoughtcrime/securesms/Release.java
@@ -3,5 +3,5 @@ package org.thoughtcrime.securesms;
 public class Release {
 
 //  public static final String PUSH_URL = "https://textsecure-service.whispersystems.org";
-  public static final String PUSH_URL = "http://192.168.87.108:8080";
+  public static final String PUSH_URL = "https://192.168.87.108:8443";
 }
