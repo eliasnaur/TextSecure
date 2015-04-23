6efa6d23a3188a12d4756764d1d965babfe161da
Elias Naur <elias.naur@gmail.com>
2015-04-23 13:41:42 +0200

    Boot listener

---
+++ src/org/thoughtcrime/securesms/service/BootCompletedReceiver.java
@@ -0,0 +1,13 @@
+package org.thoughtcrime.securesms.service;
+
+import android.content.Intent;
+import android.content.Context;
+
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+public class BootCompletedReceiver extends WakefulBroadcastReceiver {
+    @Override public void onReceive(Context context, Intent intent) {
+        Intent service = new Intent(MessageRetrievalService.ACTION_KEEPALIVE, null, context, MessageRetrievalService.class);
+        WakefulBroadcastReceiver.startWakefulService(context, service);
+    }
+}
