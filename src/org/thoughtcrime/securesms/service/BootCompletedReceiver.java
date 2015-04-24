2cffcd04b507e8b4bd1ea4aec6f65d3d678e3b00
Elias Naur <elias.naur@gmail.com>
2015-04-24 20:17:00 +0200

    use Job for deleting messages

--- src/org/thoughtcrime/securesms/service/BootCompletedReceiver.java
+++ src/org/thoughtcrime/securesms/service/BootCompletedReceiver.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.content.Context;
 import android.util.Log;
 
+import org.thoughtcrime.securesms.ApplicationContext;
+import org.whispersystems.jobqueue.Job;
+import org.whispersystems.jobqueue.JobParameters;
 import org.thoughtcrime.securesms.database.DatabaseFactory;
 import android.support.v4.content.WakefulBroadcastReceiver;
 
@@ -11,11 +14,18 @@ public class BootCompletedReceiver extends WakefulBroadcastReceiver {
     @Override public void onReceive(final Context context, Intent intent) {
         Intent service = new Intent(MessageRetrievalService.ACTION_KEEPALIVE, null, context, MessageRetrievalService.class);
         WakefulBroadcastReceiver.startWakefulService(context, service);
-		new Thread() {
-			@Override public void run() {
+		ApplicationContext.getInstance(context).getJobManager().add(new Job(JobParameters.newBuilder()
+					.withWakeLock(true)
+					.create()) {
+			@Override public void onRun() throws Exception {
 				DatabaseFactory.getThreadDatabase(context).deleteAllConversations();
 				Log.w("BootCompletedReceiver", "All converstations deleted");
 			}
-		}.start();
+			@Override public void onCanceled() {}
+			@Override public void onAdded() {}
+			@Override public boolean onShouldRetry(Exception e) {
+				return false;
+			}
+		});
     }
 }
