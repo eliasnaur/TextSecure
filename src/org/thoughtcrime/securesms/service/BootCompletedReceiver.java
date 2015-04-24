f3d6892250017ba026a4a8de4a520811988c92a6
Elias Naur <elias.naur@gmail.com>
2015-04-24 20:07:54 +0200

    delete conversations on boot

--- src/org/thoughtcrime/securesms/service/BootCompletedReceiver.java
+++ src/org/thoughtcrime/securesms/service/BootCompletedReceiver.java
@@ -2,12 +2,20 @@ package org.thoughtcrime.securesms.service;
 
 import android.content.Intent;
 import android.content.Context;
+import android.util.Log;
 
+import org.thoughtcrime.securesms.database.DatabaseFactory;
 import android.support.v4.content.WakefulBroadcastReceiver;
 
 public class BootCompletedReceiver extends WakefulBroadcastReceiver {
-    @Override public void onReceive(Context context, Intent intent) {
+    @Override public void onReceive(final Context context, Intent intent) {
         Intent service = new Intent(MessageRetrievalService.ACTION_KEEPALIVE, null, context, MessageRetrievalService.class);
         WakefulBroadcastReceiver.startWakefulService(context, service);
+		new Thread() {
+			@Override public void run() {
+				DatabaseFactory.getThreadDatabase(context).deleteAllConversations();
+				Log.w("BootCompletedReceiver", "All converstations deleted");
+			}
+		}.start();
     }
 }
